22d52b273521c92561394ed0f8a72dda28a0fbae
dukedotdev <github@dukedot.dev>
2023-11-12 03:43:25 -0500

    Move Gear, Characters, & Game Mechs categories

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,9 +1,3 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
 import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
@@ -12,24 +6,16 @@ const config = {
   tagline: 'MQ Fandom Wiki Mirror',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
   url: 'https://magiquest.wiki',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'magiquest', // Usually your GitHub org/user name.
-  projectName: 'wiki-mirror', // Usually your repo name.
+  organizationName: 'magiquest',
+  projectName: 'wiki-mirror',
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -43,8 +29,6 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/MagiQuest/wiki-mirror/tree/main/',
         },
