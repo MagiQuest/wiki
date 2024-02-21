27095feb2c08ad68fcdaf395615497b05dad3e5b
Taylor Worrell <github@dukedot.dev>
2024-02-21 07:07:26 -0500

    Add image zoom plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,6 +21,10 @@ const config = {
     locales: ['en'],
   },
 
+  plugins: [
+    'docusaurus-plugin-image-zoom',
+  ],
+
   presets: [
     [
       'classic',
@@ -42,85 +46,94 @@ const config = {
     ],
   ],
 
-  themeConfig:
+  themeConfig: {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'MQ Wiki',
-        logo: {
-          alt: 'MQ Wiki Logo',
-          src: 'img/site-assets/MQ.png',
+    navbar: {
+      title: 'MQ Wiki',
+      logo: {
+        alt: 'MQ Wiki Logo',
+        src: 'img/site-assets/MQ.png',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Wiki',
         },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Wiki',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/magiquest/wiki',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/magiquest/wiki',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Our Site',
+          items: [
+            {
+              label: 'Wiki',
+              to: '/',
+            },
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.com/invite/6e4whagCph',
+            },
+            {
+              label: 'Reddit',
+              href: 'https://www.reddit.com/r/MagiQuest',
+            },
+            {
+              label: 'Fandom',
+              href: 'https://magiquest.fandom.com',
+            },
+          ],
+        },
+        {
+          title: 'magiquest.wiki',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/magiquest',
+            },
+            {
+              label: 'BlueSky',
+              href: 'https://bsky.app/profile/magiquest.wiki',
+            },
+          ],
+        },
+      ],
+      copyright: `Community content is available under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC-BY-SA</a> unless otherwise noted. Built with <a href="https://docusaurus.io">Docusaurus</a>.<br><a href="https://github.com/MagiQuest/wiki/commits/main/">magiquest.wiki v0.6.1 + 73</a>`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+    zoom: {
+      selector: '.markdown > img',
+      background: {
+        light: 'rgb(255, 255, 255)',
+        dark: 'rgb(50, 50, 50)'
       },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Our Site',
-            items: [
-              {
-                label: 'Wiki',
-                to: '/',
-              },
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Discord',
-                href: 'https://discord.com/invite/6e4whagCph',
-              },
-              {
-                label: 'Reddit',
-                href: 'https://www.reddit.com/r/MagiQuest',
-              },
-              {
-                label: 'Fandom',
-                href: 'https://magiquest.fandom.com',
-              },
-            ],
-          },
-          {
-            title: 'magiquest.wiki',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/magiquest',
-              },
-              {
-                label: 'BlueSky',
-                href: 'https://bsky.app/profile/magiquest.wiki',
-              },
-            ],
-          },
-        ],
-        copyright: `Community content is available under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC-BY-SA</a> unless otherwise noted. Built with <a href="https://docusaurus.io">Docusaurus</a>.<br><a href="https://github.com/MagiQuest/wiki/commits/main/">magiquest.wiki v0.6.1 + 73</a>`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
-};
+      config: {
+        // options you can specify via https://github.com/francoischalifour/medium-zoom#usage
+      }
+    }
+  },
+}
 
 module.exports = config;
 
